71991ec74fb4412ab229bdaf02afc6c3d7911ae5
GiangNT63 <giangnt078@gmail.com>
2022-01-25 11:29:45 +0700

    [DEV] - Add remove duplicate sections/items for extension Publisher

--- Sources/JDiffableDataSource/Extensions/Publisher+Extension.swift
+++ Sources/JDiffableDataSource/Extensions/Publisher+Extension.swift
@@ -12,7 +12,8 @@ public extension Publisher {
     func assign<S: JSectiontable>(
         to dataSource: JTableViewDiffableDataSource<S.Item>
     ) -> AnyCancellable where Output == [S], Failure == Never {
-        return self.sink(receiveValue: { [weak dataSource] sections in
+        return self.removeDuplicates()
+            .sink(receiveValue: { [weak dataSource] sections in
             dataSource?.update(sections)
         })
     }
@@ -20,7 +21,8 @@ public extension Publisher {
     func assign<I: JItemable>(
         to dataSource: JTableViewDiffableDataSource<I>
     ) -> AnyCancellable where Output == [I], Failure == Never {
-        return self.sink(receiveValue: { [weak dataSource] items in
+        return self.removeDuplicates()
+            .sink(receiveValue: { [weak dataSource] items in
             dataSource?.update(items)
         })
     }
